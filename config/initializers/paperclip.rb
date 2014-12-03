e3a718c9fe4c1a96496939a8faf6237989ba5a3d
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-12-03 16:26:24 -0200

    Cleanup paperclip config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,9 @@
-Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:bucket] = ENV['AMAZON_S3_BUCKET']
-Paperclip::Attachment.default_options[:s3_permissions] = :public_read
-Paperclip::Attachment.default_options[:s3_credentials] = {
-  :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
-}
+Paperclip::Attachment.default_options.merge!({
+  :storage => :s3,
+  :bucket => ENV['AMAZON_S3_BUCKET'],
+  :s3_permissions => :public_read,
+  :s3_credentials => {
+    :access_key_id => ENV['AMAZON_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']
+  }
+}) unless Rails.env.test?

c00f529d3839aa5f9b8754d68567b1036a97c3ae
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-12-03 11:22:18 -0200

    Removed locale param from contents_controller

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,28 +1,20 @@
 class ContentsController < ApplicationController
   before_action :set_content, only: [:show, :edit, :update, :destroy]
 
-  # GET /contents
-  # GET /contents.json
   def index
     @contents = Content.all
   end
 
-  # GET /contents/1
-  # GET /contents/1.json
   def show
   end
 
-  # GET /contents/new
   def new
     @content = Content.new
   end
 
-  # GET /contents/1/edit
   def edit
   end
 
-  # POST /contents
-  # POST /contents.json
   def create
     @content = Content.new(content_params)
 
@@ -37,8 +29,6 @@ class ContentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contents/1
-  # PATCH/PUT /contents/1.json
   def update
     respond_to do |format|
       if @content.update(content_params)
@@ -51,8 +41,6 @@ class ContentsController < ApplicationController
     end
   end
 
-  # DELETE /contents/1
-  # DELETE /contents/1.json
   def destroy
     @content.destroy
     respond_to do |format|
@@ -62,13 +50,11 @@ class ContentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_content
       @content = Content.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def content_params
-      params.require(:content).permit(:title, :description, :locale)
+      params.require(:content).permit(:title, :description)
     end
 end

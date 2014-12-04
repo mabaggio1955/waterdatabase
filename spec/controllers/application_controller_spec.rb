0091754548bb571288d030e53010e069b72ff20a
Paulo Pires <taralloqueiroiz@gmail.com>
2014-12-04 10:10:28 -0200

    application_controller 100% covered

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -18,5 +18,30 @@ RSpec.describe ApplicationController, :type => :controller do
         it { expect(controller.send(:current_user)).to be_nil }
       end
     end
+    describe 'user_signed_in?' do
+      context 'logged_in' do
+        before { session[:user_id] = user.id }
+        it { expect(controller.send(:user_signed_in?)).to be_truthy }
+      end
+      context 'logged_out' do
+        before { session[:user_id] = nil }
+        it { expect(controller.send(:user_signed_in?)).to be_falsey }
+      end
+    end
+    describe 'authenticate!' do
+      context 'logged_in' do
+        before { session[:user_id] = user.id }
+          it { expect(controller.send(:authenticate!)).to be_truthy }
+        end
+        context 'logged_out' do
+          before { session[:user_id] = nil }
+          it {
+            redirect = double('redirect')
+            expect(controller).to receive(:redirect_to).with(root_path, notice: "Você precisa estar logado...").and_return(redirect)
+            expect(controller.send(:authenticate!)).to eq(redirect)
+          }
+        end
+      end
+    end
   end
 end

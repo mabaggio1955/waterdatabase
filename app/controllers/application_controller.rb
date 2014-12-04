94480b177fc8b576cbff84d38569ecea522a0ea5
Paulo Pires <taralloqueiroiz@gmail.com>
2014-12-04 10:47:37 -0200

    removing the befor filter for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :user_signed_in?
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= ::User.find(session[:user_id]) if session[:user_id]
+  rescue ActiveRecord::RecordNotFound
+    session.delete(:user_id)
+    nil
   end
 
   def user_signed_in?

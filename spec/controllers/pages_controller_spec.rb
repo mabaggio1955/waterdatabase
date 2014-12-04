a9fe797632f5620e4b6b9b6e03d2f53f2fa8d78f
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-12-04 10:50:33 -0200

    Created pages#dashboard

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe PagesController, :type => :controller do
     end
   end
 
+  describe "GET dashboard" do
+    it "returns http success" do
+      get :dashboard
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end

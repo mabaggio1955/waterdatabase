1e1ba07554bab5ce25ae69fa74c5ae7df2b32fbb
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-12-04 12:33:10 -0200

    Test for document#file_direct_url=

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,7 +4,6 @@ class Document < ActiveRecord::Base
   validates :content, presence: true
   belongs_to :content
 
-  # download_from_direct_url_with_delay :file
   def file_direct_url=(value)
     open(value, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE) do |file|
       self.attributes = {
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Document, :type => :model do
+  include ActionDispatch::TestProcess
+
   describe "validations" do
     it { is_expected.to have_attached_file(:file) }
     it { is_expected.to validate_attachment_content_type(:file) }
@@ -9,5 +11,17 @@ RSpec.describe Document, :type => :model do
 
   describe "associations" do
     it { is_expected.to belong_to(:content) }
+  end
+
+  describe "file_direct_url=" do
+    let(:file_path) do
+      Rails.root.join("spec", "fixtures", "test.jpg")
+    end
+
+    before do
+      subject.file_direct_url = file_path
+    end
+
+    it { expect(subject.file_file_name).to eq("test.jpg") }
   end
 end

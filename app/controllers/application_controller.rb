7735817f7997d608f316808b96f5296de89a3195
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-12-05 01:04:15 -0200

    Locale swift

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   ensure_security_headers # See more: https://github.com/twitter/secureheaders
+  before_action :set_locale
 
   helper_method :current_user, :user_signed_in?
 
@@ -19,6 +20,14 @@ class ApplicationController < ActionController::Base
 
   def authenticate!
     user_signed_in? || redirect_to(root_path, notice: "Você precisa estar logado...")
+  end
+
+  def default_url_options(options={})
+    { :locale => ((I18n.locale == I18n.default_locale) ? nil : I18n.locale) }
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'pages/dashboard'
-  get 'pages/home'
+  scope "(:locale)" do
+    get 'pages/dashboard'
+    get 'pages/home'
 
-  resources :contents do
-    resources :documents, only: [:destroy, :create]
+    resources :contents do
+      resources :documents, only: [:destroy, :create]
+    end
+
+    match "/auth/:provider/callback" => "sessions#create", as: :auth_callback, via: :get
+    match "/auth/failure" => "sessions#failure", as: :auth_failure, via: :get
+    match "/logout" => "sessions#destroy", as: :logout, via: :get
+    resources :sessions, only: [:create, :destroy]
   end
 
-  match "/auth/:provider/callback" => "sessions#create", as: :auth_callback, via: :get
-  match "/auth/failure" => "sessions#failure", as: :auth_failure, via: :get
-  match "/logout" => "sessions#destroy", as: :logout, via: :get
-  resources :sessions, only: [:create, :destroy]
-
   root 'pages#home'
+  get '/:locale' => 'contents#index'
 end

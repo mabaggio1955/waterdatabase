e9d5ae2cbd1a65da4204ae1f1ef581b60e816583
Cacau Azevedo <cacauinbox@gmail.com>
2014-12-05 17:18:25 -0200

    Categories list.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def home
-    redirect_to categories_path if user_signed_in?
+    if user_signed_in?
+      redirect_to categories_path if user_signed_in?
+    else
+      render layout: "landing"
+    end
   end
 end

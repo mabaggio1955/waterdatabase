87fe8bd2d687d948c33d48c14826cf5e2210531f
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-12-05 11:00:08 -0200

    Tests for locale

--- spec/routing/contents_routing_spec.rb
+++ spec/routing/contents_routing_spec.rb
@@ -5,31 +5,37 @@ RSpec.describe ContentsController, :type => :routing do
 
     it "routes to #index" do
       expect(:get => "/contents").to route_to("contents#index")
+      expect(:get => "/pt-BR/contents").to route_to("contents#index", :locale => "pt-BR")
     end
 
     it "routes to #new" do
       expect(:get => "/contents/new").to route_to("contents#new")
+      expect(:get => "/pt-BR/contents/new").to route_to("contents#new", :locale => "pt-BR")
     end
 
     it "routes to #show" do
       expect(:get => "/contents/1").to route_to("contents#show", :id => "1")
+      expect(:get => "/pt-BR/contents/1").to route_to("contents#show", :id => "1", :locale => "pt-BR")
     end
 
     it "routes to #edit" do
       expect(:get => "/contents/1/edit").to route_to("contents#edit", :id => "1")
+      expect(:get => "/pt-BR/contents/1/edit").to route_to("contents#edit", :id => "1", :locale => "pt-BR")
     end
 
     it "routes to #create" do
       expect(:post => "/contents").to route_to("contents#create")
+      expect(:post => "/pt-BR/contents/").to route_to("contents#create", :locale => "pt-BR")
     end
 
     it "routes to #update" do
       expect(:put => "/contents/1").to route_to("contents#update", :id => "1")
+      expect(:put => "/pt-BR/contents/1").to route_to("contents#update", :id => "1", :locale => "pt-BR")
     end
 
     it "routes to #destroy" do
       expect(:delete => "/contents/1").to route_to("contents#destroy", :id => "1")
+      expect(:delete => "/pt-BR/contents/1").to route_to("contents#destroy", :id => "1", :locale => "pt-BR")
     end
-
   end
 end
